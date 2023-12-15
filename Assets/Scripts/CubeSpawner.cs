51c30322a9e3b9d3d05e8ffb512ca9dc27aec876
Татьяна <145362065+leonarus@users.noreply.github.com>
2023-12-15 14:40:38 +0300

    First Commit

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public int rowSize = 20;
+    public int columnSize = 20;
+    public float spawnInterval = 0.5f;
+    public float spawnDistanceX = 1f; 
+    public float spawnDistanceZ = 1f;
+
+    public float colorChangeInterval = 0.2f;
+    public float colorChangeDuration = 0.5f;
+    
+    private void Start()
+    {
+        StartCoroutine(SpawnCubes());
+    }
+
+    IEnumerator SpawnCubes()
+    {
+        
+        float startX = -40f;
+        float startZ = -40f;
+        
+        for (int row = 0; row < rowSize; row++)
+        {
+            for (int column = 0; column < columnSize; column++)
+            {
+                Vector3 spawnPosition = new Vector3(startX + row * spawnDistanceX, 0, startZ + column * spawnDistanceZ);
+                Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+    }
+
+    public void ChangeColors()
+    {
+        StartCoroutine(ChangeColorsCoroutine());
+    }
+
+    IEnumerator ChangeColorsCoroutine()
+    {
+        var newColor = GetRandomColor();
+
+        foreach (var cube in GameObject.FindGameObjectsWithTag("Cube"))
+        {
+            StartCoroutine(ChangeColor(cube, newColor));
+            yield return new WaitForSeconds(colorChangeInterval);
+        }
+    }
+
+    IEnumerator ChangeColor(GameObject cube, Color newColor)
+    {
+        Material cubeMaterial = cube.GetComponent<Renderer>().material;
+        Color startColor = cubeMaterial.color;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < colorChangeDuration)
+        {
+            cubeMaterial.color = Color.Lerp(startColor, newColor, elapsedTime / colorChangeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        cubeMaterial.color = newColor;
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(Random.value, Random.value, Random.value);
+    }
+}
